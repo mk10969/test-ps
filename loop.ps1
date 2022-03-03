f7ae919237693f701937c63f74ed3afe7a0b6215
mk10969 <43040394+mk10969@users.noreply.github.com>
2022-03-03 16:24:46 +0900

    Create loop.ps1

---
+++ loop.ps1
@@ -0,0 +1,11 @@
+add-type -AssemblyName System.Windows.Forms
+
+while (true) {
+    # shift key 押す
+    [System.Windows.Forms.SendKeys]::SendWait("+")
+    # 日付Get
+    $DateTime = Get-Date
+    Write-Host $DateTime
+    Start-Sleep -s 240
+}
+
